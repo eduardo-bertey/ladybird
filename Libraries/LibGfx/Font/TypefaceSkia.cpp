ede566a1343fc42120f92ce57b9f3ea4c69bc334
eduardo-bertey <eduardo-bertey@users.noreply.github.com>
2026-08-11 18:31:39 -0300

    LibGfx: fix SkFontMgr_New_Android call for newer skia (2-arg scanner API)
    skia 148 requires a SkFontScanner argument; pass the FreeType scanner
    and enable the freetype feature for the Android skia build.

--- Libraries/LibGfx/Font/TypefaceSkia.cpp
+++ Libraries/LibGfx/Font/TypefaceSkia.cpp
@@ -20,6 +20,7 @@
 #include <core/SkTypeface.h>
 #if defined(AK_OS_ANDROID)
 #    include <ports/SkFontMgr_android.h>
+#    include <ports/SkFontScanner_FreeType.h>
 #elif defined(AK_OS_WINDOWS)
 #    include <ports/SkFontMgr_empty.h>
 #    include <ports/SkTypeface_win.h>
@@ -88,7 +89,7 @@ static SkFontMgr& font_manager()
         }
 #endif
 #if defined(AK_OS_ANDROID)
-        font_manager = SkFontMgr_New_Android(nullptr);
+        font_manager = SkFontMgr_New_Android(nullptr, SkFontScanner_Make_FreeType());
 #elif defined(AK_OS_WINDOWS)
         if (Gfx::FontDatabase::the().force_freetype_rasterization())
             font_manager = SkFontMgr_New_Custom_Empty();
