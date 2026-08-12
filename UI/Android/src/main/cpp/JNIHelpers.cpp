40952bf5e19a71be24fc1aab27c86635f0ff09bf
eduardo-bertey <eduardo-bertey@users.noreply.github.com>
2026-08-11 21:37:27 -0300

    Ladybird: replace removed AK::utf8_to_utf16 with Utf16String API
    Build a contiguous UTF-16 code unit buffer from Utf16String::from_utf8()
    for the JNI NewString call; handles both ASCII and UTF-16 storage.

--- UI/Android/src/main/cpp/JNIHelpers.cpp
+++ UI/Android/src/main/cpp/JNIHelpers.cpp
@@ -5,14 +5,22 @@
  */
 
 #include "JNIHelpers.h"
-#include <AK/Utf16View.h>
+#include <AK/Utf16String.h>
+#include <AK/Vector.h>
 
 namespace Ladybird {
 
 jstring JavaEnvironment::jstring_from_ak_string(String const& str)
 {
-    auto as_utf16 = MUST(AK::utf8_to_utf16(str.code_points()));
-    return m_env->NewString(reinterpret_cast<jchar const*>(as_utf16.data.data()), as_utf16.data.size());
+    auto as_utf16 = Utf16String::from_utf8(str);
+    auto const& view = as_utf16.utf16_view();
+
+    Vector<u16> code_units;
+    code_units.ensure_capacity(view.length_in_code_units());
+    for (size_t i = 0; i < view.length_in_code_units(); ++i)
+        code_units.append(view.code_unit_at(i));
+
+    return m_env->NewString(reinterpret_cast<jchar const*>(code_units.data()), code_units.size());
 }
 
 }
